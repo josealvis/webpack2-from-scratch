ef27e8a8c86f08e001c4a10d489c0ad9a37c86d4
jose alvis <jalvis1491@gmail.com>
2018-03-23 03:34:12 +0300

    Agrega webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import notify from "./alertFunc";
 
 
 var message = _.join(["webpack2", "vuejs", "lodash"], ", ");
-var text  =`This is a text for ${message} ok `;
+var text  =` ${message} ok `;
 
 
 var app = new Vue({
     el:"#app",
     data:{
-        message : "Hello "+ message,
+        message : "Hello node",
         text
     }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
-const path = require('path');
-var webpack = require('webpack');
-var FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+let path = require('path');
+let webpack = require('webpack');
+let FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// the path(s) that should be cleaned
+let pathsToClean = ['dist'];
+
+// the clean options to use
+let cleanOptions = {
+  root:     __dirname,
+  verbose:  true,
+  dry:      false
+}
 
 
 var configuration = {
@@ -9,15 +21,24 @@ var configuration = {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
+       devServer: {
+        contentBase: './dist'
+     },
     module:{
       rules:[
-        { test: /\.s[ac]ss/, use:['style-loader', 'css-loader', 'sass-loader']},
+        { test: /\.s[ac]ss$/, use:['style-loader', 'css-loader', 'sass-loader']},
         { test: /\.css$/, use: ['style-loader', 'css-loader']},
         { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
         ]
     },
     plugins: [
-      new FriendlyErrorsWebpackPlugin()
+      new FriendlyErrorsWebpackPlugin(),
+      new CleanWebpackPlugin(pathsToClean, cleanOptions),
+      new HtmlWebpackPlugin({ 
+        filename: 'index.html',
+        template: 'src/index.html'
+      })
     ]
   };
 
